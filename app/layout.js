08d39a40ac8c45b7c1499a2690e6e221c8bd26e8
mwangimuringi <mwangimuringi10@gmail.com>
2024-08-31 14:45:20 +0300

    Initialized the repo

---
+++ app/layout.js
@@ -0,0 +1,21 @@
+import { Roboto } from "next/font/google";
+import "./globals.css";
+
+const roboto = Roboto({ subsets: ['latin'], weight: ['400', '500', '700'] });
+
+export const metadata = {
+  title: "Tamu Bites App",
+  description: "Ladha Safi, Delivery Mtaani",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={roboto.className}>
+        <main className="max-w-4xl mx-auto p-4">
+        {children}
+        </main>
+      </body>
+    </html>
+  );
+}
