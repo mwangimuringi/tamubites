f84ab4f709a814af39277a5d6ee9b60a2eff87de
mwangimuringi <mwangimuringi10@gmail.com>
2024-08-31 15:28:28 +0300

    ...

--- app/layout.js
+++ app/layout.js
@@ -15,8 +15,6 @@ export default function RootLayout({ children }) {
         <main className="max-w-4xl mx-auto p-4">
         {children}
         </main>
-
-        
       </body>
     </html>
   );
