ce25001cff474ab400c09311b9cfe7da424c51bd
mwangimuringi <mwangimuringi10@gmail.com>
2024-09-01 23:15:22 +0300

    Register page component

--- app/register/page.js
+++ app/register/page.js
@@ -1,37 +1,77 @@
-// import Header from "@/components/layout/Header";
-// import Hero from "@/components/layout/Hero";
-// import HomeMenu from "@/components/layout/HomeMenu";
-// import SectionHeaders from "@/components/layout/SectionHeaders";
+"use client";
+import {signIn} from "next-auth/react";
+import Image from "next/image";
+import Link from "next/link";
+import {useState} from "react";
 
-// export default function Home() {
-//   return (
-//     <>
-//       <Hero />
-//       <HomeMenu />
-//       <section className="text-center my-16" id="about">
-//         <SectionHeaders
-//           subHeader={'Our story'}
-//           mainHeader={'About us'}
-//         />
-//         <div className="text-gray-500 max-w-md mx-auto mt-4 flex flex-col gap-4">
-//           <p>
-//             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni minima odit recusandae. Illum ipsa non repudiandae? Eum ipsam iste quos suscipit tempora? Aperiam esse fugiat inventore laboriosam officiis quam rem!
-//           </p>
-//           <p>At consectetur delectus ducimus est facere iure molestias obcaecati quaerat vitae voluptate? Aspernatur dolor explicabo iste minus molestiae pariatur provident quibusdam saepe?</p>
-//           <p>Laborum molestias neque nulla obcaecati odio quia quod reprehenderit sit vitae voluptates? Eos, tenetur.</p>
-//         </div>
-//       </section>
-//       <section className="text-center my-8" id="contact">
-//         <SectionHeaders
-//           subHeader={'Don\'t hesitate'}
-//           mainHeader={'Contact us'}
-//         />
-//         <div className="mt-8">
-//           <a className="text-4xl underline text-gray-500" href="tel:+46738123123">
-//             +254 738 123 123
-//           </a>
-//         </div>
-//       </section>
-//     </>
-//   )
-// }
+export default function RegisterPage() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [creatingUser, setCreatingUser] = useState(false);
+  const [userCreated, setUserCreated] = useState(false);
+  const [error, setError] = useState(false);
+  
+  async function handleFormSubmit(ev) {
+    ev.preventDefault();
+    setCreatingUser(true);
+    setError(false);
+    setUserCreated(false);
+    const response = await fetch('/api/register', {
+      method: 'POST',
+      body: JSON.stringify({email, password}),
+      headers: {'Content-Type': 'application/json'},
+    });
+    if (response.ok) {
+      setUserCreated(true);
+    }
+    else {
+      setError(true);
+    }
+    setCreatingUser(false);
+  }
+  return (
+    <section className="mt-8">
+      <h1 className="text-center text-primary text-4xl mb-4">
+        Register
+      </h1>
+      {userCreated && (
+        <div className="my-4 text-center">
+          User created.<br />
+          Now you can{' '}
+          <Link className="underline" href={'/login'}>Login &raquo;</Link>
+        </div>
+      )}
+      {error && (
+        <div className="my-4 text-center">
+          An error has occurred.<br />
+          Please try again later
+        </div>
+      )}
+      <form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
+        <input type="email" placeholder="email" value={email}
+               disabled={creatingUser}
+               onChange={ev => setEmail(ev.target.value)} />
+        <input type="password" placeholder="password" value={password}
+               disabled={creatingUser}
+                onChange={ev => setPassword(ev.target.value)}/>
+        <button type="submit" disabled={creatingUser}>
+          Register
+        </button>
+        <div className="my-4 text-center text-gray-500">
+          or login with provider
+        </div>
+        <button
+          onClick={() => signIn('google', {callbackUrl:'/'})}
+          className="flex gap-4 justify-center">
+          <Image src={'/google.png'} alt={''} width={24} height={24} />
+          Login with google
+        </button>
+        <div className="text-center my-4 text-gray-500 border-t pt-4">
+          Existing account?{' '}
+          <Link className="underline" href={'/login'}>Login here &raquo;</Link>
+        </div>
+      </form>
+    </section>
+  );
+}
