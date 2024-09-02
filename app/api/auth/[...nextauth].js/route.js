d538787a44cb80a49729352f70dcc469a9d59600
mwangimuringi <mwangimuringi10@gmail.com>
2024-09-02 22:42:28 +0300

    Created categories

--- app/api/auth/[...nextauth].js/route.js
+++ app/api/auth/[...nextauth].js/route.js
@@ -9,7 +9,6 @@ import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@auth/mongodb-adapter"
 
 
-
 export const authOptions = {
   secret: process.env.SECRET,
   adapter: MongoDBAdapter(clientPromise),
