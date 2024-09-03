7a9375149d0532b69f1ffda2ae31248443859bc3
mwangimuringi <mwangimuringi10@gmail.com>
2024-09-03 16:22:22 +0300

    ...

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,10 @@
-import {AppProvider} from "@/components/AppContext";
+import { AppProvider } from "@/components/AppContext";
 import Header from "@/components/layout/Header";
 import { Roboto } from "next/font/google";
 import "./globals.css";
-import {Toaster} from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 
-const roboto = Roboto({ subsets: ['latin'], weight: ['400', '500', '700'] });
+const roboto = Roboto({ subsets: ["latin"], weight: ["400", "500", "700"] });
 
 export const metadata = {
   title: "Tamu Bites App",
@@ -14,18 +14,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
-    <body className={roboto.className}>
-      <main className="max-w-4xl mx-auto p-4">
-        <AppProvider>
-          <Toaster />
-          <Header />
-          {children}
-          <footer className="border-t p-8 text-center text-gray-500 mt-16">
-            &copy; 2024 All rights reserved
-          </footer>
-        </AppProvider>
-      </main>
-    </body>
-  </html>
+      <body className={roboto.className}>
+        <main className="max-w-4xl mx-auto p-4">
+          <AppProvider>
+            <Toaster />
+            <Header />
+            {children}
+            <footer className="border-t p-8 text-center text-gray-500 mt-16">
+              &copy; 2024 All rights reserved
+            </footer>
+          </AppProvider>
+        </main>
+      </body>
+    </html>
   );
 }
