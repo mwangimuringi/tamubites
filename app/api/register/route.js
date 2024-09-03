1a23c35231c15e78d7db48266297a604b415a620
mwangimuringi <mwangimuringi10@gmail.com>
2024-09-03 18:16:28 +0300

    Listing products and menu

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -1,4 +1,4 @@
-import {User} from "@/models/User";
+import { User } from '../../models/User'; // Adjust the path as needed
 import bcrypt from "bcryptjs";
 import mongoose from "mongoose";
 
--- app/models/User.js
+++ app/models/User.js
@@ -1,10 +1,10 @@
-import {model, models, Schema} from "mongoose";
+import { model, models, Schema } from 'mongoose';
 
 const UserSchema = new Schema({
-  name: {type: String},
-  email: {type: String, required: true, unique: true},
-  password: {type: String},
-  image: {type: String},
-}, {timestamps: true});
+  name: { type: String, trim: true }, // Added trim to remove extra spaces
+  email: { type: String, required: true, unique: true, trim: true, lowercase: true }, // Trim and lowercase for better consistency
+  password: { type: String, required: true }, // Make password required
+  image: { type: String, default: '' }, // Set a default value for image
+}, { timestamps: true });
 
-export const User = models?.User || model('User', UserSchema);
+export const User = models.User || model('User', UserSchema);
