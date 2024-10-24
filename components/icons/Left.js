02ad18c681b53d1d94c9b7d2d9b970d39f71b86d
mwangimuringi <mwangimuringi10@gmail.com>
2024-10-24 20:53:51 +0300

    ////

--- components/icons/Left.js
+++ components/icons/Left.js
@@ -3,6 +3,13 @@ export default function Left({ className = "w-6 h-6" }) {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
+
+
+
+
+
+
+      
       viewBox="0 0 24 24"
       strokeWidth={1.5}
       stroke="currentColor"
