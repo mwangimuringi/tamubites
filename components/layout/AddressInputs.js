a09c2005710f617d73be285eed87c28fb109b337
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-09 17:59:03 +0300

    Improved layout and spacing with TailwindCSS utilities.

--- components/layout/AddressInputs.js
+++ components/layout/AddressInputs.js
@@ -1,5 +1,6 @@
 // Reusable InputField Component
 const InputField = ({
+  id,
   label,
   value,
   placeholder,
@@ -7,14 +8,18 @@ const InputField = ({
   disabled,
   type = "text",
 }) => (
-  <div>
-    <label>{label}</label>
+  <div className="mb-4">
+    <label htmlFor={id} className="block text-sm font-medium text-gray-700">
+      {label}
+    </label>
     <input
+      id={id}
       type={type}
       placeholder={placeholder}
       value={value || ""}
       onChange={onChange}
       disabled={disabled}
+      className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
     />
   </div>
 );
@@ -28,8 +33,9 @@ export default function AddressInputs({
   const { phone, streetAddress, postalCode, city, country } = addressProps;
 
   return (
-    <>
+    <form className="space-y-4">
       <InputField
+        id="phone"
         label="Phone"
         type="tel"
         placeholder="Phone number"
@@ -38,21 +44,24 @@ export default function AddressInputs({
         disabled={disabled}
       />
       <InputField
-        label="Street address"
+        id="streetAddress"
+        label="Street Address"
         placeholder="Street address"
         value={streetAddress}
         onChange={(ev) => setAddressProp("streetAddress", ev.target.value)}
         disabled={disabled}
       />
-      <div className="grid grid-cols-2 gap-2">
+      <div className="grid grid-cols-2 gap-4">
         <InputField
-          label="Postal code"
+          id="postalCode"
+          label="Postal Code"
           placeholder="Postal code"
           value={postalCode}
           onChange={(ev) => setAddressProp("postalCode", ev.target.value)}
           disabled={disabled}
         />
         <InputField
+          id="city"
           label="City"
           placeholder="City"
           value={city}
@@ -61,12 +70,13 @@ export default function AddressInputs({
         />
       </div>
       <InputField
+        id="country"
         label="Country"
         placeholder="Country"
         value={country}
         onChange={(ev) => setAddressProp("country", ev.target.value)}
         disabled={disabled}
       />
-    </>
+    </form>
   );
 }
