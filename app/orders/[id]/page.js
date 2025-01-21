5b67a39306f9c6792c23872d07a0797b19b6b0a2
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-21 23:27:41 +0300

    Implement cart clearing functionality.

--- app/orders/[id]/page.js
+++ app/orders/[id]/page.js
@@ -1,27 +1,16 @@
-import { useParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
+import { CartContext } from "@/components/AppContext";
 
 export default function OrderPage() {
-  const { id } = useParams();
-  const [order, setOrder] = useState();
-  const [loadingOrder, setLoadingOrder] = useState(true);
+  const { clearCart } = useContext(CartContext);
 
   useEffect(() => {
-    if (id) {
-      setLoadingOrder(true);
-      fetch("/api/orders?_id=" + id).then((res) => {
-        res.json().then((orderData) => {
-          setOrder(orderData);
-          setLoadingOrder(false);
-        });
-      });
+    if (typeof window.console !== "undefined") {
+      if (window.location.href.includes("clear-cart=1")) {
+        clearCart();
+      }
     }
   }, []);
 
-  return (
-    <section>
-      {loadingOrder && <div>Loading order...</div>}
-      {order && <div>Order fetched: {JSON.stringify(order)}</div>}
-    </section>
-  );
+  return <section>Order Page with cart clearing logic.</section>;
 }
