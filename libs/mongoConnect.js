571c2fff70fb0e561d4deb0b8c07a8989e9b12d6
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-23 22:56:35 +0300

    Removed unused variables.

--- libs/mongoConnect.js
+++ libs/mongoConnect.js
@@ -1,4 +1,3 @@
-// This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
 import { MongoClient } from "mongodb"
 
 if (!process.env.MONGO_URL) {
@@ -12,8 +11,6 @@ let client
 let clientPromise;
 
 if (process.env.NODE_ENV === "development") {
-  // In development mode, use a global variable so that the value
-  // is preserved across module reloads caused by HMR (Hot Module Replacement).
   if (!global._mongoClientPromise) {
     client = new MongoClient(uri, options)
     global._mongoClientPromise = client.connect()
